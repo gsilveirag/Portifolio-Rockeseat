d78d34f7cc56a78608c15e0f3b8fe948a14dba00
gsilveirag <gustavo.silveira.goncalves@gmail.com>
2023-05-31 16:17:53 -0300

    Mais um projeto, e arrumado alguns bugs.

--- script.js
+++ script.js
@@ -41,6 +41,31 @@ const imagens = [
  })
 
 
- // Script Portifolio
+ const sobreMim = document.getElementById("sobreMim"); 
+
+sobreMim.addEventListener("click", function() {
+  minhaDiv.innerHTML = "";
+  // Muda a cor de Fundo
+  
+  minhaDiv.style.backgroundColor = "rgb(230, 227, 248)"
+  
+
+  const texto = document.createElement("p")
+  texto.textContent = "Após mais de 10 anos de experiência em bancos, sigo para uma transição de carreira, saindo da zona de conforto, em busca de um sonho na área de tecnologia.Hoje para alcançar meus objetivos, estou finalizando o curso de Analise e Desenvolvimento de Sistemas, na Universidade de Vila Velha.Tenho usado plataformas auxiliares no meu desenvolvimento como, Udemy, balta.io, Rockseat...Atualmente meu foco esta no curso da onebitcode : Full Stack Javascript.E estou cursando o curso do Senac, focado na trilha de desenvolvimento." 
+
+  //adicionar o texto na div
+  minhaDiv.appendChild(texto);
+
+  // Remove os elementos e limpa a div 
+  setTimeout(() => {
+    minhaDiv.innerHTML = "";
+    minhaDiv.style.backgroundColor = 'transparent'
+  }, 10000); 
+
+})
+
+
+
+
 
 
